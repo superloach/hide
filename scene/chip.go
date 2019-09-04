d786227b987044bf68eb7af4ae663e94eb7056fd
Terra Brown <superloach42@gmail.com>
2019-09-04 16:28:57 -0400

    more chip improvements

--- scene/chip.go
+++ scene/chip.go
@@ -21,18 +21,20 @@ type ChipEnt struct {
 }
 
 // chipmunk physics scene
-type ChipmunkScene struct {
+type ChipScene struct {
 	Scene
 	Game    GameIface
 	Space   *cp.Space
-	Gravity float64
 	Ents    []*ChipEnt
 }
 
 func MakeChipmunkScene(game GameIface) (*ChipmunkScene, int) {
 	s := ChipmunkScene{}
 	s.Game = game
-	s.Gravity = 333
+	s.Space = cp.NewSpace()
+	s.Space.Iterations = 1
+	s.Space.SetGravity(cp.Vector{0, 333})
+
 	id := s.Game.AddScene(&s)
 	return &s, id
 }
@@ -75,7 +77,7 @@ func (s *ChipmunkScene) MakeCirc(image *ebiten.Image, x, y float64, mass float64
 	body.SetPosition(cp.Vector{x, y})
 	ce.Body = body
 
-	shape := cp.NewCircle(body, r, cp.Vector{r, -2 * r})
+	shape := cp.NewCircle(body, r, cp.Vector{0, 0})
 	ce.Shape = shape
 
 	s.Ents = append(s.Ents, &ce)
@@ -83,12 +85,6 @@ func (s *ChipmunkScene) MakeCirc(image *ebiten.Image, x, y float64, mass float64
 }
 
 func (s *ChipmunkScene) Step() {
-	if s.Space == nil {
-		s.Space = cp.NewSpace()
-		s.Space.Iterations = 2
-		s.Space.SetGravity(cp.Vector{0, s.Gravity})
-	}
-
 	s.Space.Step(1.0 / float64(ebiten.MaxTPS()))
 }
 
