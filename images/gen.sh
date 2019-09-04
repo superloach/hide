19e11ba014779bb44e6b11399dfa2e264667a0b5
Terra Brown <superloach42@gmail.com>
2019-09-04 13:10:47 -0400

    minor gen.sh fixes

--- images/gen.sh
+++ images/gen.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 IMAGEDIR=$PWD/$(dirname $0)
 cd $IMAGEDIR
-rm -rf *.go ./f2bs
+rm -rf ./f2bs
 curl -Ls https://raw.githubusercontent.com/hajimehoshi/file2byteslice/master/main.go > f2bs.go
 go build f2bs.go
 rm f2bs.go
 for ext in png jpg jpeg gif; do
 	echo $ext\s:
-	for file in $(find . -name *.$ext); do
+	for file in $(find . -name \*.$ext); do
 		file=$(basename $file)
 		name=$(echo $file | sed "s,.$ext\$,,")_$ext
 		var=$(echo $name | sed -r 's,(^|_)([a-z]),\U\2,g')
